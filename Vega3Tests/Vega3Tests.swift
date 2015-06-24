68be83f17f28882fc10ed94e86616464b5eda602
Nevinniy Vladimir <nevinniyvladimir@MacNevinniy.local>
2015-06-24 11:08:07 +0300

    Initial Commit

---
+++ Vega3Tests/Vega3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Vega3Tests.swift
+//  Vega3Tests
+//
+//  Created by Nevinniy Vladimir on 24.06.15.
+//  Copyright (c) 2015 Nevinniy Vladimir. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Vega3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
